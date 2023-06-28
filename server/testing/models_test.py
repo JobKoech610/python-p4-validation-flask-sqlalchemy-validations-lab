a8865ef1df638a96db53b530c2f0799e921fac14
Job koech <jobkoech610@gmail.com>
2023-06-28 23:55:02 +0300

    done with validations

--- server/testing/models_test.py
+++ server/testing/models_test.py
@@ -1,7 +1,5 @@
 import pytest
-from sqlalchemy.exc import IntegrityError, CompileError
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy.exc import IntegrityError
 from app import app
 from models import db, Author, Post
 
@@ -11,40 +9,28 @@ class TestAuthor:
 
     def test_requires_name(self):
         '''requires each record to have a name.'''
-
         with app.app_context():
             with pytest.raises(ValueError):
-                author = Author(name = '', phone_number = '1231144321')
-            db.session.query(Author).delete()
-            db.session.query(Post).delete()
-            db.session.commit()
+                author = Author(name='', phone_number='1231144321')
+                db.session.add(author)
+                db.session.commit()
 
     def test_requires_unique_name(self):
         '''requires each record to have a unique name.'''
         with app.app_context():
-
-            author_a = Author(name = 'Ben', phone_number = '1231144321')
-            author_b = Author(name = 'Ben', phone_number = '1231144321')
+            author_a = Author(name='Ben', phone_number='1231144321')
+            author_b = Author(name='Ben', phone_number='1231144321')
             with pytest.raises(IntegrityError):
                 db.session.add(author_a)
                 db.session.add(author_b)
                 db.session.commit()
-                db.session.query(Author).delete()
-                db.session.commit()
-
 
     def test_requires_ten_digit_phone_number(self):
         '''requires each phone number to be exactly ten digits.'''
-
         with app.app_context():
-
             with pytest.raises(ValueError):
                 author = Author(name="Jane Author", phone_number="3311")
-                author2 = Author(name="Jane Author", phone_number="3312212121212121")
-
                 db.session.add(author)
-                db.session.add(author2)
-
                 db.session.commit()
 
 
@@ -53,35 +39,25 @@ class TestPost:
 
     def test_requires_title(self):
         '''requires each post to have a title.'''
-
         with app.app_context():
-            content_string = "This is content" * 150
-
-            post = Post(content=content_string, category='Non-Fiction')
-            with pytest.raises(IntegrityError):
+            with pytest.raises(ValueError):
+                post = Post(content="Some content", category='Non-Fiction')
                 db.session.add(post)
-                db.session.commit()
-                db.session.query(Post).delete()
                 db.session.commit()
 
     def test_content_length(self):
         '''requires content to be greater than or equal 250 characters long.'''
-
         with app.app_context():
             content_string = "This is content" * 2
-
             with pytest.raises(ValueError):
                 post = Post(title='Secret, Why I love programming.', content=content_string, category='Non-Fiction')
                 db.session.add(post)
                 db.session.commit()
-                db.session.query(Post).delete()
-                db.session.commit()
 
-    def test_content_length(self):
+    def test_content_too_short(self):
         '''Content too short test. Less than 250 chars.'''
-
         with app.app_context():
-            content_string = "This is content" * 2
+            content_string = "This is content"
             with pytest.raises(ValueError):
                 post = Post(title='Secret, Why I love programming.', content=content_string, category='Non-Fiction')
                 db.session.add(post)
@@ -89,31 +65,48 @@ class TestPost:
 
     def test_summary_length(self):
         '''Summary too long test. More than 250 chars.'''
-
         with app.app_context():
             content_string = "This is content" * 150
             summary_string = "T" * 250
             with pytest.raises(ValueError):
-                post = Post(title='Secret, Why I love programming.', content=content_string, summary= summary_string, category='Non-Fiction')
+                post = Post(title='Secret, Why I love programming.', content=content_string, summary=summary_string, category='Non-Fiction')
                 db.session.add(post)
                 db.session.commit()
 
     def test_category(self):
         '''Incorrect category test'''
-
         with app.app_context():
             content_string = "This is content" * 150
             with pytest.raises(ValueError):
                 post = Post(title='Secret, Why I love programming.', content=content_string, category='Banana')
                 db.session.add(post)
-                db.session.commit()
+                db.session.commit()            
 
+    # def test_category(self):
+    #  '''Incorrect category test'''
+
+    #  with app.app_context():
+    #     content_string = "This is content" * 150
+    #     with pytest.raises(ValueError, match='Invalid category'):
+    #         post = Post(title='Secret, Why I love programming.', content=content_string, category='Banana')
+    #         db.session.add(post)
+    #         db.session.commit()
+
+
+    # def test_category(self):
+    #     '''Incorrect category test'''
+    #     with app.app_context():
+    #         content_string = "This is content" * 150
+    #         with pytest.raises(ValueError):
+    #             post = Post(title='Secret, Why I love programming.', content=content_string, category='Banana')
+    #             db.session.add(post)
+    #             db.session.commit()
 
     def test_clickbait(self):
         '''Test clickbait validator for title.'''
         with app.app_context():
             content_string = "This is content" * 150
             with pytest.raises(ValueError):
-                post = Post(title='Why I love programming.', content=content_string, category='Fiction')
+                post = Post(title='Why I love clickbait programming.', content=content_string, category='Fiction')
                 db.session.add(post)
                 db.session.commit()
